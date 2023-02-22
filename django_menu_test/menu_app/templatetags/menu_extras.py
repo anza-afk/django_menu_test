c22bdca9917029615083cced3ad9e888cb6552b3
anza-afk <zapokami@gmail.com>
2023-02-22 14:50:00 +0300

    new logic

--- django_menu_test/menu_app/templatetags/menu_extras.py
+++ django_menu_test/menu_app/templatetags/menu_extras.py
@@ -1,42 +1,75 @@
 from django import template
-from django.urls import reverse
+from django.urls import reverse, NoReverseMatch
 from menu_app.models import Menu, MenuItem
 
 register = template.Library()
 
 
 @register.inclusion_tag('menu_app/menu_template.html', takes_context=True)
-def draw_menu(context, name, parent = None):
-    if parent:
-        menu = context['menu']
+def draw_menu(context, name):
 
-        menu = MenuItem.objects.get(name=name)
+    menu_query = MenuItem.objects.select_related('menu').filter(menu__name=name)
+    menu_items = list(menu_query.filter(parent=None))
+
+    try:
+        current_url = context['request'].path
+    except KeyError:
+        current_url = '/'
+
+    current_item = menu_query.filter(url=current_url).first()
+
+    active = [current_item.id]
+
+    children = [item for item in menu_query if item.parent == current_item]
+    return {
+        'menu': menu_query,
+        'menu_items': menu_items,
+        'children': children,
+        'active': active,
+    }
+
+@register.inclusion_tag('menu_app/menu_template.html', takes_context=True)
+def draw_menu_item(context, name):
+
+    if 'menu_query' in context:
+        menu_query = context['menu_query']
+        current_item = menu_query.filter(name=name).first()
+
     else:
         menu_query = MenuItem.objects.select_related('menu').all()
-        # menu = Menu.objects.select_related('parent').all()
-        
-        try:
-            current_url = context['request'].path
-        except KeyError:
-            current_url = '/'
-        current_menu_item = menu_query.filter(url=current_url).first()
-        menu = []
+        current_item = None
 
-        for item in menu_query:
-            if 'http' in item.url:
-                url = item.url
-            else:
-                url = reverse(item.url)
+    try:
+        current_url = context['request'].path
+    except KeyError:
+        current_url = '/'
 
-            menu.append({
-                'item':  item
-            })
-        #     print(item)
-        #     local_context.append(item)
+    active_item = menu_query.filter(url=current_url).first()
+
+    if not current_item:
+        current_item = active_item
+
+    def get_parents(menu_item):
+        if menu_item.parent:
+            return get_parents(menu_item.parent).append(menu_item.parent.id)
+        else:
+            return []
+    
+    active = get_parents(active_item)
+    active.append(active_item.id)
+
+    print('active///////')
+    print(active)
+
+    children = [item for item in menu_query if item.parent == current_item]
+
+    print('children///////')
+    print(children)
 
     return {
-        'menu': menu,
-
-        # 'current_menu_item': current_menu_item,
-        # 'active': active,
+        'menu_item': current_item,
+        'children': children,
+        'active': active,
+        'menu_query': menu_query
     }
+
